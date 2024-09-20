62a90dba9a69040a38c76ab71f5f7ca2074fda53
Yves Iraguha <76212304+yvesP123@users.noreply.github.com>
2024-09-20 11:54:22 +0200

    Update App.js
    Update the url

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/export/:id" element={<ExportPage />} />
+        <Route path="/export/:id/:company_id" element={<ExportPage />} />
       </Routes>
     </Router>
   );
